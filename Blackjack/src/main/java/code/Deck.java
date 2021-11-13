64f300d03a6fa9399c54e670cf915d11fff9bf28
guillem-martinez <guillem.martinezar@e-campus.uab.cat>
2021-11-13 17:28:29 +0100

    Deck.SwapCardsV1

--- Blackjack/src/main/java/code/Deck.java
+++ Blackjack/src/main/java/code/Deck.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
-  List<Card> deck = new ArrayList<Card>(52);
+  public List<Card> deck = new ArrayList<Card>();
 
-  public Deck(){}
-  private void swapCards(){}
+  public Deck(){
+
+    for (int i=1;i<=13;i++){
+      deck.add(new Card(i,"C"));
+      deck.add(new Card(i,"D"));
+      deck.add(new Card(i,"H"));
+      deck.add(new Card(i,"S"));
+    }
+  }
+  public void swapCards(int pos1, int pos2){
+    Card aux1;
+    Card aux2;
+    aux1 = deck.get(pos1);
+    aux2 = deck.get(pos2);
+    deck.set(pos1, aux2);
+    deck.set(pos2, aux1);
+  }
   public void shuffle(){}
   public Card getCard(int pos){return null;}
   public Card nextCard(){return null;}
-
 
 
 }
