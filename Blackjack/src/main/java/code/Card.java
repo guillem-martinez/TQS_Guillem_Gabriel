bb9bf60f36d438d989001efd75c6f507399a1fb5
guillem-martinez <guillem.martinezar@e-campus.uab.cat>
2021-11-13 13:20:06 +0100

    Card and V1Deck_Test

--- Blackjack/src/main/java/code/Card.java
+++ Blackjack/src/main/java/code/Card.java
@@ -1,14 +1,23 @@
 package code;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Card {
-
-  public int num;
-  private static final String suits[] = {"C", "D", "H", "S"};
-
-  public void Card(){
+  private final List<Integer> values = Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13);
+  private int num;
+  private final List<String> suits = Arrays.asList("C", "D", "H", "S");
+  private String suit;
+  public Card(int n, String s){
+    num = n;
+    suit = s;
   }
 
-  public String getSuit(){return "";}
-  public int getNum(){return 0;}
+  public String getSuit(){return suit;}
+  public int getNum(){return num;}
+  public List<Integer> getValues() {return values;}
+  public List<String> getSuits() {return suits;}
+  public void setSuit(String suit){this.suit = suit;}
+  public void setNum(int num){this.num = num;}
 
 }
--- Blackjack/src/test/java/BlackjackTests/Deck_Test.java
+++ Blackjack/src/test/java/BlackjackTests/Deck_Test.java
@@ -1,22 +1,69 @@
 package BlackjackTests;
-
-
+import code.Card;
 import code.Deck;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
 
 
 
 public class Deck_Test {
 
-  Deck deck;
+  @Test
+  public void deckConstructorTest() {
 
-  @BeforeEach
-  void setUp(){
-    deck = new Deck();
+    Deck deckTest = new Deck();
+    int capacity = deckTest.deck.size();
+
+    assertEquals(52,capacity);
+
+    boolean find;
+    Card cardTest = new Card(10,"S");
+    find = deckTest.deck.contains(cardTest);
+
+    assertTrue(true == find);
+
+    cardTest.setNum(15);
+    find = deckTest.deck.contains(cardTest);
+
+    assertTrue(find == false);
+
   }
+
+  @Test
+  public void swapCardsTestV1(){
+    int pos1 = 40;
+    int pos2 = 20;
+    Deck deckTest = new Deck();
+
+    assertTrue(pos1 <= 52);
+    assertTrue(pos2 <= 52);
+
+    Card aux = deckTest.deck.get(pos1); //2 picas
+
+    deckTest.swapCards(pos1, pos2); // pos 1 tindra 4 cors i pos2 2 picas
+
+    assertEquals(deckTest.deck.get(pos2).getNum(),aux.getNum());
+    //assertEquals(deckTest.deck.get(pos1).getNum(),deckTest.deck.get(pos2).getNum());
+
+    //assertEquals(deckTest.deck.get(pos2).getSuit(),deckTest.deck.get(pos1).getSuit());
+    //assertEquals(deckTest.deck.get(pos1).getSuit(),deckTest.deck.get(pos2).getSuit());
+
+  }
+  @Test
+  public void shuffleTest(){
+
+    Deck deckTest = new Deck();
+
+  }
+
+
 
 
 
