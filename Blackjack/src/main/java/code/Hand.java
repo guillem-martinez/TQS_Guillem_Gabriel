ad9cbcb2f8911525c53952768bd335e53e94a8b6
guillem-martinez <guillem.martinezar@e-campus.uab.cat>
2021-11-14 10:57:05 +0100

    First Hand&Hand_Test

--- Blackjack/src/main/java/code/Hand.java
+++ Blackjack/src/main/java/code/Hand.java
@@ -1,12 +1,36 @@
 package code;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hand {
   private static final int NUM_MAX_CARDS = 11;
-  Card cards[];
+  private int numCards = 0;
+  public List<Card> cards;
+
+  public Hand(){
+    cards = new ArrayList<Card>(NUM_MAX_CARDS);
+  }
 
   public int calculateTotal(){return 0;}
-  public void addCard(Card card){}
-  public void clearHeand(){}
+  public boolean addCard(Card card){
+    /* V1
+    cards.add(card);
+    numCards++;
+    return true;
+    */
+
+    if (cards.contains(card)){
+      return false;
+    }
+    else{
+      cards.add(card);
+      numCards++;
+      return true;
+    }
+
+  }
+  public void clearHand(){}
+  public int getNumCards(){return this.numCards;}
 }
--- Blackjack/src/test/java/BlackjackTests/Hand_Test.java
+++ Blackjack/src/test/java/BlackjackTests/Hand_Test.java
@@ -1,15 +1,57 @@
 package BlackjackTests;
+import code.Card;
 import code.Hand;
-import org.junit.jupiter.api.BeforeEach;
+import org.mockito.*;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 public class Hand_Test {
 
-  Hand hand;
 
-  @BeforeEach
-  void SetUp(){
-    hand = new Hand();
+  @Test
+  public void handConstructorTest(){
+    Hand handTest = new Hand();
+
+    assertNotNull(handTest);
+    assertNotNull(handTest.cards.size());
   }
+
+  @Test
+  public void calculateTotalTest(){
+    Hand handTest = new Hand();
+    int sumTest = 0;
+    handTest.calculateTotal();
+
+  }
+
+  @Mock
+  private Card cardMock = new Card(10,"S");
+
+  @Test
+  public void addCardTestV1(){
+    Hand handTest = new Hand();
+    int size = handTest.cards.size();
+
+    handTest.addCard(cardMock);
+
+    assertNotEquals(size,handTest.cards.size());
+
+
+
+  }
+  @Test
+  public void addCardTestV2(){
+    Hand handTest = new Hand();
+    int size = handTest.cards.size();
+
+    handTest.addCard(cardMock);
+    assertEquals(false,handTest.addCard(cardMock));
+    assertNotEquals(size,handTest.cards.size());
+  }
+
 
 }
